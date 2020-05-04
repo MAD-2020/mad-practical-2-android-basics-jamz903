f86f4441526cb7d3e5a6d020bcd2d189ade03651
jamz903 <64241899+jamz903@users.noreply.github.com>
2020-05-04 10:06:02 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,12 @@
 package sg.edu.np.WhackAMole;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -14,11 +17,12 @@ public class MainActivity extends AppCompatActivity {
         - Feel free to modify the function to suit your program.
     */
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.v(TAG, "Finished Pre-Initialisation!");
     }
 
@@ -26,13 +30,95 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
         setNewMole();
+        TextView score = (TextView)findViewById(R.id.score);
+        score.setText("0");
+        updateScore();
         Log.v(TAG, "Starting GUI!");
     }
 
 
     public void setNewMole()
     {
+        Button left = (Button)findViewById(R.id.left);
+        Button center = (Button)findViewById(R.id.center);
+        Button right = (Button)findViewById(R.id.right);
         Random ran = new Random();
+
         int randomLocation = ran.nextInt(3);
+        if(randomLocation == 0){
+            left.setText("*");
+            center.setText("O");
+            right.setText("O");
+
+        }
+        else if(randomLocation == 1){
+            left.setText("O");
+            center.setText("*");
+            right.setText("O");
+        }
+        else{
+            left.setText("O");
+            center.setText("O");
+            right.setText("*");
+        }
+
+    }
+
+    public void updateScore()
+    {
+        final Button left = (Button)findViewById(R.id.left);
+        final Button center = (Button)findViewById(R.id.center);
+        final Button right = (Button)findViewById(R.id.right);
+        final TextView score = (TextView)findViewById(R.id.score);
+        final int[] count = {0};
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Left Button Clicked!");
+                if(left.getText() == "*"){
+                    Log.v(TAG, "Hit, Score added!");
+                    count[0] += 1;
+                }
+                else{
+                    Log.v(TAG, "Missed, Score deducted!");
+                    count[0] -= 1;
+                }
+                score.setText(String.valueOf(count[0]));
+                setNewMole();
+            }
+        });
+        center.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Middle Button Clicked!");
+                if(center.getText() == "*"){
+                    Log.v(TAG, "Hit, Score added!");
+                    count[0] += 1;
+                }
+                else{
+                    Log.v(TAG, "Missed, Score deducted!");
+                    count[0] -= 1;
+                }
+                score.setText(String.valueOf(count[0]));
+                setNewMole();
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Right Button Clicked!");
+                if(right.getText() == "*"){
+                    Log.v(TAG, "Hit, Score added!");
+                    count[0] += 1;
+                }
+                else{
+                    Log.v(TAG, "Missed, Score deducted!");
+                    count[0] -= 1;
+                }
+                score.setText(String.valueOf(count[0]));
+                setNewMole();
+            }
+        });
+
     }
 }
